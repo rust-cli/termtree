c175c5841d98d97c498d4a8d93635a2ec33afd69
Ed Page <eopage@gmail.com>
2021-10-06 11:59:19 -0500

    feat: Opt-in rendering for multiline strings
    In `assert_cmd`, we render assertion failures with treeline and
    frequently have multi-line strings.  This will make it easier to see the
    content.
    
    Fixes #4

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,36 @@ use std::fmt::{self, Display};
 pub struct Tree<D: Display> {
     root: D,
     leaves: Vec<Tree<D>>,
+    multiline: bool,
 }
 
 impl<D: Display> Tree<D> {
     pub fn new(root: D, leaves: Vec<Tree<D>>) -> Tree<D> {
-        Tree { root, leaves }
+        Tree {
+            root,
+            leaves,
+            multiline: false,
+        }
     }
 
     pub fn root(root: D) -> Tree<D> {
         Tree {
             root,
             leaves: Vec::new(),
+            multiline: false,
         }
+    }
+
+    /// Ensure all lines for `root` are indented
+    pub fn with_multiline(mut self, yes: bool) -> Self {
+        self.multiline = yes;
+        self
+    }
+
+    /// Ensure all lines for `root` are indented
+    pub fn set_multiline(&mut self, yes: bool) -> &mut Self {
+        self.multiline = yes;
+        self
     }
 
     pub fn push(&mut self, leaf: Tree<D>) -> &mut Self {
@@ -32,18 +50,35 @@ impl<D: Display> Tree<D> {
     ) -> fmt::Result {
         for (i, leaf) in leaves.iter().enumerate() {
             let last = i >= leaves.len() - 1;
-            // print single line
-            for s in &spaces {
-                if *s {
-                    write!(f, "    ")?;
-                } else {
-                    write!(f, "|   ")?;
+            let mut prefix = if last { "└──" } else { "├──" };
+
+            if leaf.multiline {
+                let rest_prefix = if last { "   " } else { "|  " };
+                debug_assert_eq!(prefix.chars().count(), rest_prefix.chars().count());
+
+                let root = leaf.root.to_string();
+                for line in root.lines() {
+                    // print single line
+                    for s in &spaces {
+                        if *s {
+                            write!(f, "    ")?;
+                        } else {
+                            write!(f, "|   ")?;
+                        }
+                    }
+                    writeln!(f, "{} {}", prefix, line)?;
+                    prefix = rest_prefix;
                 }
-            }
-            if last {
-                writeln!(f, "└── {}", leaf.root)?;
             } else {
-                writeln!(f, "├── {}", leaf.root)?;
+                // print single line
+                for s in &spaces {
+                    if *s {
+                        write!(f, "    ")?;
+                    } else {
+                        write!(f, "|   ")?;
+                    }
+                }
+                writeln!(f, "{} {}", prefix, leaf.root)?;
             }
 
             // recurse
@@ -81,6 +116,38 @@ mod tests {
             r#"foo
 └── bar
     └── baz
+"#
+        )
+    }
+
+    #[test]
+    fn render_tree_with_multiple_leaves() {
+        let tree = Tree::new("foo", vec![Tree::root("bar"), Tree::root("baz")]);
+        assert_eq!(
+            format!("{}", tree),
+            r#"foo
+├── bar
+└── baz
+"#
+        )
+    }
+
+    #[test]
+    fn render_tree_with_multiline_leaf() {
+        let tree = Tree::new(
+            "foo",
+            vec![
+                Tree::root("hello\nworld").with_multiline(true),
+                Tree::root("goodbye\nworld").with_multiline(true),
+            ],
+        );
+        assert_eq!(
+            format!("{}", tree),
+            r#"foo
+├── hello
+|   world
+└── goodbye
+    world
 "#
         )
     }
