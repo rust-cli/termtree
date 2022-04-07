1131c1aab76006370b7e8de61bb8435bb1c368b5
Ed Page <eopage@gmail.com>
2022-04-07 10:06:47 -0500

    refactor: Reduce recompilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use std::collections::VecDeque;
 use std::fmt::{self, Display};
 use std::rc::Rc;
@@ -194,56 +197,5 @@ impl GlyphPalette {
 impl Default for GlyphPalette {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Tree;
-    #[test]
-    fn render_tree_root() {
-        let tree = Tree::new("foo");
-        assert_eq!(format!("{}", tree), "foo\n")
-    }
-
-    #[test]
-    fn render_tree_with_leaves() {
-        let tree = Tree::new("foo").with_leaves([Tree::new("bar").with_leaves(["baz"])]);
-        assert_eq!(
-            format!("{}", tree),
-            r#"foo
-└── bar
-    └── baz
-"#
-        )
-    }
-
-    #[test]
-    fn render_tree_with_multiple_leaves() {
-        let tree = Tree::new("foo").with_leaves(["bar", "baz"]);
-        assert_eq!(
-            format!("{}", tree),
-            r#"foo
-├── bar
-└── baz
-"#
-        )
-    }
-
-    #[test]
-    fn render_tree_with_multiline_leaf() {
-        let tree = Tree::new("foo").with_leaves([
-            Tree::new("hello\nworld").with_multiline(true),
-            Tree::new("goodbye\nworld").with_multiline(true),
-        ]);
-        assert_eq!(
-            format!("{}", tree),
-            r#"foo
-├── hello
-│   world
-└── goodbye
-    world
-"#
-        )
     }
 }
