913d8d64b9efdd4ea0db34c358cd14c534a2d8b2
Ed Page <eopage@gmail.com>
2024-07-09 10:19:35 -0500

    test: Show current glyph behavior

--- src/tests.rs
+++ src/tests.rs
@@ -61,3 +61,63 @@ foo
 "#]]
     );
 }
+
+#[test]
+fn render_custom_glyphs() {
+    let root = GlyphPalette {
+        middle_item: "[mid  ]",
+        last_item: "[last ]",
+        item_indent: "[indent ]",
+
+        middle_skip: "[mskip]",
+        last_skip: "[lskip]",
+        skip_indent: "[iskip  ]",
+    };
+    let middle = GlyphPalette {
+        middle_item: "(mid  )",
+        last_item: "(last )",
+        item_indent: "(indent )",
+
+        middle_skip: "(mskip)",
+        last_skip: "(lskip)",
+        skip_indent: "(iskip  )",
+    };
+
+    let tree = Tree::new("node 1").with_glyphs(root).with_leaves([
+        Tree::new("node 1.1"),
+        Tree::new("node 1.2"),
+        Tree::new("node 1.3").with_leaves([
+            Tree::new("node 1.3.1").with_glyphs(middle),
+            Tree::new("node 1.3.2").with_glyphs(middle),
+            Tree::new("node 1.3.3")
+                .with_glyphs(middle)
+                .with_leaves(["node 1.3.3.1", "node 1.3.3.2"]),
+        ]),
+        Tree::new("node 1.4").with_leaves([
+            Tree::new("node 1.4.1"),
+            Tree::new("node 1.4.2"),
+            Tree::new("node 1.4.3").with_leaves(["node 1.4.3.1", "node 1.4.3.2"]),
+        ]),
+    ]);
+    assert_data_eq!(
+        format!("{}", tree),
+        str![[r#"
+node 1
+├── node 1.1
+├── node 1.2
+├── node 1.3
+[mskip][iskip  ](mid  )(indent )node 1.3.1
+[mskip][iskip  ](mid  )(indent )node 1.3.2
+[mskip][iskip  ](last )(indent )node 1.3.3
+[mskip][iskip  ][lskip][iskip  ]├── node 1.3.3.1
+[mskip][iskip  ][lskip][iskip  ]└── node 1.3.3.2
+└── node 1.4
+[lskip][iskip  ]├── node 1.4.1
+[lskip][iskip  ]├── node 1.4.2
+[lskip][iskip  ]└── node 1.4.3
+[lskip][iskip  ][lskip][iskip  ]├── node 1.4.3.1
+[lskip][iskip  ][lskip][iskip  ]└── node 1.4.3.2
+
+"#]]
+    );
+}
