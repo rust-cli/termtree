948af2951a12105ce446e6cc57ea4a3930241f87
Ed Page <eopage@gmail.com>
2024-07-09 10:08:37 -0500

    test: Port to snapbox

--- src/tests.rs
+++ src/tests.rs
@@ -1,32 +1,45 @@
+use snapbox::assert_data_eq;
+use snapbox::str;
+
 use super::*;
 
 #[test]
 fn render_tree_root() {
     let tree = Tree::new("foo");
-    assert_eq!(format!("{}", tree), "foo\n");
+    assert_data_eq!(
+        format!("{}", tree),
+        str![[r#"
+foo
+
+"#]]
+    );
 }
 
 #[test]
 fn render_tree_with_leaves() {
     let tree = Tree::new("foo").with_leaves([Tree::new("bar").with_leaves(["baz"])]);
-    assert_eq!(
+    assert_data_eq!(
         format!("{}", tree),
-        r#"foo
+        str![[r#"
+foo
 └── bar
     └── baz
-"#
+
+"#]]
     );
 }
 
 #[test]
 fn render_tree_with_multiple_leaves() {
     let tree = Tree::new("foo").with_leaves(["bar", "baz"]);
-    assert_eq!(
+    assert_data_eq!(
         format!("{}", tree),
-        r#"foo
+        str![[r#"
+foo
 ├── bar
 └── baz
-"#
+
+"#]]
     );
 }
 
@@ -36,13 +49,15 @@ fn render_tree_with_multiline_leaf() {
         Tree::new("hello\nworld").with_multiline(true),
         Tree::new("goodbye\nworld").with_multiline(true),
     ]);
-    assert_eq!(
+    assert_data_eq!(
         format!("{}", tree),
-        r#"foo
+        str![[r#"
+foo
 ├── hello
 │   world
 └── goodbye
     world
-"#
+
+"#]]
     );
 }
